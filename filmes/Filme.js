0322f5dae4552e1e47006ff889606b590dc5c5c5
aylton <ayltondias703@gmail.com>
2024-08-26 23:18:51 -0300

    definição do models e filmes

--- filmes/Filme.js
+++ filmes/Filme.js
@@ -2,7 +2,20 @@ const Sequelize = require("sequelize");
 const Connection = require("../database/database");
 
 const Filme = Connection.define("filmes", {
-
+    titulo: {
+        typeof: Sequelize.STRING,
+        allowerNull: false
+    },
+    genero: {
+        typeof: Sequelize.STRING,
+        allowerNull: false      
+    },
+    descricao: {
+        tuppeof: Sequelize.TEXT,
+        allowerNull: false
+    }
 });
+
+//Filme.sync({force: true});
 
 module.exports = Filme;
